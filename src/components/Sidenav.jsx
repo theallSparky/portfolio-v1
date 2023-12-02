4dad49503556b3c8d5c05e2259cf07486f4f7ff8
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 14:56:13 -0600

    Updated project with AiOutlineMail icon

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -28,7 +28,7 @@ const Sidenav = () => {
 
                     </a>
                     <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 curdor:pointer hover:scale-110 ease-in duration-200'>
-                        <GrProjects size={20} />
+                        <AiOutlineProject size={20} />
                         <span className='pl-4'>Projects</span>
 
                     </a>
