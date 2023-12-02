a7bd67b2425a9c77a7538a5c143bf6acfac0785a
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 15:58:54 -0600

    Started on Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div id='main'>
+        <img 
+            className='w-full h-screen object-cover'
+            src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+        />
+        <div className='w-full h-screen absolute top-0 left-0 bg-white/10'>
+
+        </div>
+    </div>
   )
 }
 
