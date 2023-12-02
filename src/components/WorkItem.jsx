ac54286bbea5a6affff889be4d7077622691416b
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 17:56:02 -0600

    Finished that styling

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -5,7 +5,7 @@ const WorkItem = ({year, title, duration, details}) => {
     <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>
       <li className='mb-10 ml-4'>
         <div className='absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white'/>
-        <p>
+        <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
           <span>{year}</span>
           <span>{title}</span>
           <span>{duration}</span>
