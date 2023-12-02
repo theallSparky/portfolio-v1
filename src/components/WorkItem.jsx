ddfd8b04c2e1dd1a3db25516ae26261c77c30e8d
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 17:54:06 -0600

    More WorkItem styling

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const WorkItem = ({year, title, duration, details}) => {
   return (
     <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>
-      <li className=''>
-        <div />
+      <li className='mb-10 ml-4'>
+        <div className='absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white'/>
         <p>
           <span>{year}</span>
           <span>{title}</span>
