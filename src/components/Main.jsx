c271cd770e268de766ee48b504f1cf63d101b98d
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 16:01:40 -0600

    Added static text for react-type-animation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,10 @@ const Main = () => {
             src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
         />
         <div className='w-full h-screen absolute top-0 left-0 bg-white/10'>
-
+        <div>
+            <h1>I'm Harrison Sparks</h1>
+            <h2></h2>
+        </div>
         </div>
     </div>
   )
