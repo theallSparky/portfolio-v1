39057660cd5f0e7e7aa06fccadde509142c59877
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 17:32:17 -0600

    Created work data, styled div container

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import WorkItem from './WorkItem'
+
+const data = [
+    {
+        year: 2022,
+        title: 'Apptricity',
+        duration: '3 months',
+        details: `I accepted a position as an accounting clerk while in school at UTA for accounting, however my schedule and communication skills at the time lead to a mutually amicable separation. I handled highly sensitive financial information for all clients of the corporation. Effectively organized thousands of paper documents for use across the entire organization`,
+    },
+    {
+        year: 2023,
+        title: `Revenue Engine`,
+        duration: '2 months, currently working!',
+        details: 'Collaborated to design and develop the core components of the web application. Learned Machine Learning and cooberated it its implementations.',
+    },
+    {
+        year: 2023,
+        title: 'General Assembly Immersive Fellow',
+        duration: '4 months',
+        details: "Engaged in a 12-week long remote full-stack web development learning experience spanning a total of 500 hours. The program's focus was to teach and provide ample experience with front-end technologies, back-end technologies, version control, and to reinforce industry wide best-practices.",
+    }
+]
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div id='work' className='max-w-[1040px] m-auto md:l-20 p-4 py-16'>
+        
+    </div>
   )
 }
 
