e929f0fafb5b4581d03a36f1dd9eea35813956c7
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 16:17:09 -0600

    Added default TypeAnimation tag

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation'
 
 const Main = () => {
   return (
@@ -11,6 +12,24 @@ const Main = () => {
         <div>
             <h1>I'm Harrison Sparks</h1>
             <h2>I'm a </h2>
+            <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'We produce food for Mice',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'We produce food for Hamsters',
+        1000,
+        'We produce food for Guinea Pigs',
+        1000,
+        'We produce food for Chinchillas',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
+
         </div>
         </div>
     </div>
