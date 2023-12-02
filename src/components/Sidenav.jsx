c9bc157e3fbdbf74b8efb64cb8039092ac9a914d
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 14:19:23 -0600

    Centered the home icon when navbar active

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -14,11 +14,11 @@ const Sidenav = () => {
         <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
         {
             nav ? (
-                <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center z-20'>
+                <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
                     <a href='#main'>
                         <AiOutlineHome size={20} />
                         <span className='pl-4'>Home</span>
-                        
+
                     </a>
                 </div>
             ) : (
