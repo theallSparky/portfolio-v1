f7df4ef0079ae3e2efe151f31908ccf59a4c0fe1
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 14:15:03 -0600

    Added more styling

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineMenu, AiOutlineHome } from 'react-icons/ai'
 
 const Sidenav = () => {
 
@@ -12,6 +12,19 @@ const Sidenav = () => {
   return (
     <div>
         <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+        {
+            nav ? (
+                <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center z-20'>
+                    <a href='#main'>
+                        <AiOutlineHome size={20} />
+                        <span className='pl-4'>Home</span>
+                        
+                    </a>
+                </div>
+            ) : (
+                <div></div>
+            )
+        }
     </div>
   )
 }
