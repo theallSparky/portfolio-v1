a54c9635076155e15c653a73562db3e4f09cb5e1
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 19:56:45 -0600

    Made the project images look SUPER NICE

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ProjectItem = ({img, title}) => {
   return (
-    <div className=''>
-        <img src={img} alt={title} />
-        <div>
-            <h3>
+    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]'>
+        <img src={img} alt={title} className='rounded-xl group-hover:opacity-10' />
+        <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+            <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
                 {title}
             </h3>
         </div>
