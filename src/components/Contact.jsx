0c1fb0cafd175fbc4b6f33fc3d279e803c8f388b
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 20:18:45 -0600

    Created basic form, imported Contant.jsx into App.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div id='contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+        <h1 className='py-4 text-4xl font-bold text-center text-[#0015be]'>Contact</h1>
+        <form action="" method='POST' encType='multipart/form-data'>
+            <div>
+                <div>
+                    <label>Name</label>
+                    <input type="text" name='name' />
+                </div>
+                <div>
+                    <label>Number</label>
+                    <input type="text" name='phone' />
+                </div>
+            </div>
+            <div>
+                <label>Email</label>
+                <input type="email" name="email" />
+            </div>
+            <div>
+                <label>Subject</label>
+                <input type="text" name="subject" />
+            </div>
+            <div>
+                <label>Message</label>
+                <textarea rows="10" name="message"></textarea>
+            </div>
+        </form>
+    </div>
   )
 }
 
