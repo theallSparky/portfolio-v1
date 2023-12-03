e2984c12517e40b48dd24711c9bc1f1194d1bf18
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 19:42:20 -0600

    Updated images with 'Img' at end of file name for clarity

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import ProjectItem from './ProjectItem'
-import sparkysmovies from '../assets/sparkysmovies.png'
-import sparkysboxgame from '../assets/sparkys-box-game.png'
-import Stopwatch from '../assets/3D Spline Stopwatch.png'
-import Sparkyscalculator from '../assets/sparkyscalculator.png'
-import Pong from '../assets/Pong.png'
-import sparkyschats from '../assets/sparkyschats.png'
+import sparkysmoviesImg from '../assets/sparkysmovies.png'
+import sparkysboxgameImg from '../assets/sparkysboxgame.png'
+import StopwatchImg from '../assets/Stopwatch.png'
+import SparkyscalculatorImg from '../assets/sparkyscalculator.png'
+import PongImg from '../assets/Pong.png'
+import sparkyschatsImg from '../assets/Sparkychats.png'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+        <h1 className='text-4xlfont-bold text-center text-[#001b5e]'>Projects</h1>
+        <p>Desc.</p>
+        <div className='grid sm:grid-cols-2 gap-12'>
+            <ProjectItem img={sparkysmoviesImg} title='Movie App' />
+            <ProjectItem img={StopwatchImg} title='Stopwatch App' />
+            <ProjectItem img={SparkyscalculatorImg} title='Calculator App' />
+            <ProjectItem img={PongImg} title='Pong App' />
+            <ProjectItem img={sparkyschatsImg} title='Chat App' />
+            <ProjectItem img={sparkysboxgameImg} title='sparkysboxgame App' />
+        </div>
+    </div>
   )
 }
 
