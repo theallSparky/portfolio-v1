4d65933cfd1a496b6a5625d4b8332ede91e0f656
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-02 19:19:01 -0600

    Added & renamed image files, imported into Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import ProjectItem from './ProjectItem'
+import sparkysmovies from '../assets/sparkysmovies.png'
+import sparkysboxgame from '../assets/sparkys-box-game.png'
+import Stopwatch from '../assets/3D Spline Stopwatch.png'
+import Sparkyscalculator from '../assets/sparkyscalculator.png'
+import Pong from '../assets/Pong.png'
+import sparkyschats from '../assets/sparkyschats.png'
 
 const Projects = () => {
   return (
