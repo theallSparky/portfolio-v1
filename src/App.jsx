53480c63e3307b7886d9a9eb56700693732b2e5d
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-04 17:18:06 -0600

    Corrected Sidenav import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Sidenav from './components/sidenav'
+import Sidenav from './components/Sidenav'
 import Main from './components/Main'
 import Work from './components/Work'
 import Projects from './components/Projects'
