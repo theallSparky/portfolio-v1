5afd7f25bffb9b43cd476fa8120fb616c428b03e
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-05 13:38:36 -0600

    Created Animations component

---
+++ src/components/Animations.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Animations = () => {
+    return (
+        <div id="animations" className='max-w-[1040px]'>
+            <h1>Animations</h1>
+        </div>
+    )
+}
