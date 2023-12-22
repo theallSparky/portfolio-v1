9b55d269835fc2122865aeae4b57c80ae4c7dac8
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-22 12:12:44 -0600

    Cleaned up contact.jsx, added gray bg on hoer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,38 +1,65 @@
-import React from 'react'
-
 const Contact = () => {
   return (
-    <div id='contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-        <h1 className='py-4 text-4xl font-bold text-center text-[#0015be]'>Contact</h1>
-        <form action="https://getform.io/f/c2aa7230-c224-4142-946c-16a6d1a74dfd" method='POST' encType='multipart/form-data'>
-            <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
-                <div className='flex flex-col'>
-                    <label className='uppercase text-sm py-2'>Name</label>
-                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='name' />
-                </div>
-                <div  className='flex flex-col'>
-                    <label className='uppercase text-sm py-2'>Phone Number</label>
-                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='phone' />
-                </div>
-            </div>
-            <div className='flex flex-col py-2'>
-                <label className='uppercase text-sm py-2'>Email</label>
-                <input className='border-2 rounded-lg p-3 flex border-gray-300' type="email" name="email" />
-            </div>
-            <div  className='flex flex-col py-2'>
-                <label className='uppercase text-sm py-2'>Subject</label>
-                <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name="subject" />
-            </div>
-            <div  className='flex flex-col py-2'>
-                <label className='uppercase text-sm py-2'>Message</label>
-                <textarea className='border-2 rounded-lg p-3 border-gray-300' rows="10" name="message"></textarea>
-            </div>
-            <button className='bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg'>
-                Send Message
-            </button>
-        </form>
+    <div
+      id="contact"
+      className="max-w-[1040px] m-auto md:pl-20 p-4 py-16 border border-solid hover:bg-slate-300 from-violet-300 to-red-800"
+    >
+      <h1 className="py-4 text-4xl font-bold text-center text-[#0015be]">
+        Contact
+      </h1>
+      <form
+        action="https://getform.io/f/c2aa7230-c224-4142-946c-16a6d1a74dfd"
+        method="POST"
+        encType="multipart/form-data"
+      >
+        <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+          <div className="flex flex-col">
+            <label className="uppercase text-sm py-2">Name</label>
+            <input
+              className="border-2 rounded-lg p-3 flex border-gray-300"
+              type="text"
+              name="name"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="uppercase text-sm py-2">Phone Number</label>
+            <input
+              className="border-2 rounded-lg p-3 flex border-gray-300"
+              type="text"
+              name="phone"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col py-2">
+          <label className="uppercase text-sm py-2">Email</label>
+          <input
+            className="border-2 rounded-lg p-3 flex border-gray-300"
+            type="email"
+            name="email"
+          />
+        </div>
+        <div className="flex flex-col py-2">
+          <label className="uppercase text-sm py-2">Subject</label>
+          <input
+            className="border-2 rounded-lg p-3 flex border-gray-300"
+            type="text"
+            name="subject"
+          />
+        </div>
+        <div className="flex flex-col py-2">
+          <label className="uppercase text-sm py-2">Message</label>
+          <textarea
+            className="border-2 rounded-lg p-3 border-gray-300"
+            rows="10"
+            name="message"
+          ></textarea>
+        </div>
+        <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg">
+          Send Message
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
